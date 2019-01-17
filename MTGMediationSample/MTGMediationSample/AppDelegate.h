325f7bf457f31d160c908d97df8d5a567615d1af
zhangChark <zhangchark@gmail.com>
2019-01-17 15:33:46 +0800

    create new project

---
+++ MTGMediationSample/MTGMediationSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTGMediationSample
+//
+//  Created by CharkZhang on 2019/1/17.
+//  Copyright © 2019 CharkZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
