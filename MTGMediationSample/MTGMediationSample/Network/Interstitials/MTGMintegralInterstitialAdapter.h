e329a12e1b69b3bd441229621a19cd3c772d406f
miao.yue <miao.yue@mobvista.com>
2019-02-12 15:25:14 +0800

    create interstitialAdapter

---
+++ MTGMediationSample/MTGMediationSample/Network/Interstitials/MTGMintegralInterstitialAdapter.h
@@ -0,0 +1,17 @@
+//
+//  MTGMintegralInterstitialAdapter.h
+//  MTGMediationSample
+//
+//  Created by ym on 2019/2/12.
+//  Copyright © 2019 CharkZhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTGMintegralInterstitialAdapter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MTGMediationSample/MTGMediationSample/Network/Interstitials/MTGMintegralInterstitialAdapter.m
@@ -0,0 +1,13 @@
+//
+//  MTGMintegralInterstitialAdapter.m
+//  MTGMediationSample
+//
+//  Created by ym on 2019/2/12.
+//  Copyright © 2019 CharkZhang. All rights reserved.
+//
+
+#import "MTGMintegralInterstitialAdapter.h"
+
+@implementation MTGMintegralInterstitialAdapter
+
+@end
